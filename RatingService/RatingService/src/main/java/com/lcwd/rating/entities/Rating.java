1090905fd7a19149d771d04296fc1ef7414d8b28
vivekdevinfo <vivekdev.info@gmail.com>
2023-01-04 13:58:50 +0530

    complete source code of working microservices

---
+++ RatingService/RatingService/src/main/java/com/lcwd/rating/entities/Rating.java
@@ -0,0 +1,24 @@
+package com.lcwd.rating.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="user_ratings")
+public class Rating {
+    @Id
+    private String ratingId;
+    private String userId;
+    private String hotelId;
+    private  int rating;
+    private  String feedback;
+}
